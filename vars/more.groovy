9e85812197072449205ca283c3d73a97298e5e09
Henry Borchers <henryborchers@yahoo.com>
2017-08-11 16:06:49 -0500

    printing commands

--- vars/more.groovy
+++ vars/more.groovy
@@ -1,8 +1,12 @@
 def call(body){
     println("Runing more.groovy")
     def config = [:]
+
+    println("body.resolveStrategy = Closure.DELEGATE_FIRST")
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
+
+    println("body()")
     body()
     println("Done running more.groovy")
 }
