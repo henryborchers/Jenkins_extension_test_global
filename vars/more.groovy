8126a709d345db2a0988cdd2bd4005336b717452
Henry Borchers <henryborchers@yahoo.com>
2017-08-11 16:12:46 -0500

    comment

--- vars/more.groovy
+++ vars/more.groovy
@@ -1,13 +1,13 @@
-def call(body){
+def call(body) {
     println("Runing more.groovy")
     def config = [:]
 
     println("body.resolveStrategy = Closure.DELEGATE_FIRST")
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-
+//    body.resolveStrategy = Closure.DELEGATE_FIRST
+//    body.delegate = config
+//
     println("config.name = ${config.name}")
-    println("body()")
-    body()
+//    println("body()")
+//    body()
     println("Done running more.groovy")
 }
