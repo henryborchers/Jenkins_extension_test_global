0a6331b641ab30207c6832934a4f7b660f7552c8
Henry Borchers <henryborchers@yahoo.com>
2017-08-11 21:39:05 -0500

    name says

--- vars/qux.groovy
+++ vars/qux.groovy
@@ -2,7 +2,7 @@ class qux implements Serializable {
     private String name
 
     def sayHi() {
-        println("HI!!!")
+        println("${name} says HI!!!")
 //        echo "echo says HI!"
     }
 
