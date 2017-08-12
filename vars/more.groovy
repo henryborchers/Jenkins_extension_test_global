8bd42a09a5c04d4aba894d90099dfd2a41d41524
Henry Borchers <henryborchers@yahoo.com>
2017-08-11 20:41:54 -0500

    clean up

--- vars/more.groovy
+++ vars/more.groovy
@@ -1,15 +1,10 @@
 def call(body) {
-    println("Runing more.groovy")
     def config = [:]
-
-    println("body.resolveStrategy = Closure.DELEGATE_FIRST")
     body.resolveStrategy = Closure.DELEGATE_FIRST
-    println("body.delegate = config")
     body.delegate = config
-//
-    println("body()")
     body()
     node {
+
         println("inside node")
         println("config.name = ${config.name}")
     }
