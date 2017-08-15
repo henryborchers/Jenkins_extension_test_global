ce3078401b6849e2842c4565ae895a08bf7f0a12
Henry Borchers <henryborchers@yahoo.com>
2017-08-15 11:54:10 -0500

    no script

--- vars/more.groovy
+++ vars/more.groovy
@@ -3,15 +3,12 @@ def call(body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
-    script {
-        body()
-        node {
+    body()
+    node {
 
-            println("inside node")
-            println("config.name = ${config.name}")
-            println("config = ${config}")
-        }
-
+        println("inside node")
+        println("config.name = ${config.name}")
+        println("config = ${config}")
     }
     println("Done running more.groovy")
 }
