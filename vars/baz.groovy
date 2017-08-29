650c0e3a48d090bf3f9971c29f73597deb32d941
Henry Borchers <henryborchers@yahoo.com>
2017-08-29 09:54:35 -0500

    map args

--- vars/baz.groovy
+++ vars/baz.groovy
@@ -1,9 +1,9 @@
-def call(message, Closure body) {
+def call(Map args, Closure body) {
     stage("Baz") {
 //        node{
         echo "Starting"
         script {
-            echo "I have a message at it is ${message}"
+            echo "I have a message at it is ${args.message}"
             println(body)
             body()
 
