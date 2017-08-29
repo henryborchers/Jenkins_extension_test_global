6e3e97c76f8d28511c71b03d0095abc79de9526d
Henry Borchers <henryborchers@yahoo.com>
2017-08-29 09:47:23 -0500

    debug messages

--- vars/baz.groovy
+++ vars/baz.groovy
@@ -1,11 +1,14 @@
 def call(Closure body) {
     stage("Baz") {
 //        node{
+        echo "Starting"
         script {
+
             println(body)
             body()
 
         }
+        echo "ending"
 //        }
 
     }
